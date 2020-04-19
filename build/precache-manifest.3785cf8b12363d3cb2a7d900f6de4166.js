fde0e2fa5d9c17fa0d21cec159c3e18fd1c10544
Jarkko Mourujarvi <jarkkom@unity3d.com>
2020-04-19 09:37:25 +0300

    3.11 puhelinluettelo full stack

---
+++ build/precache-manifest.3785cf8b12363d3cb2a7d900f6de4166.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb42fa70cf0f3a738282de0fa0290786",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cde2e080ff191dd46792",
+    "url": "/static/css/main.d2910d7f.chunk.css"
+  },
+  {
+    "revision": "8868a2df4d8e4a61f736",
+    "url": "/static/js/2.28012aa4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.28012aa4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cde2e080ff191dd46792",
+    "url": "/static/js/main.b8ad3104.chunk.js"
+  },
+  {
+    "revision": "c940798f285500efd361",
+    "url": "/static/js/runtime-main.64749484.js"
+  }
+]);
