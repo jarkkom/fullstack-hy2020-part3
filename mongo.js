cee79722f37762be2fa2e18ff7180e30fef32032
Jarkko Mourujärvi <jarkko@mourujarvi.net>
2020-05-02 13:57:46 +0300

    3.12: tietokanta komentoriviltä

---
+++ mongo.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+
+function usage() {
+    console.log('usage: mongo.js <password> [name] [phonenumber]');
+    process.exit(1);
+}
+
+if (process.argv.length != 3 && process.argv.length != 5) {
+    usage();
+}
+
+const password = process.argv[2];
+
+const mongoUrl = `mongodb+srv://fullstackhy2020:${password}@freecluster-96odi.mongodb.net/note-app?retryWrites=true&w=majority`;
+
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true});
+
+const phonebookSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+});
+
+const Phonebook = mongoose.model('Phonebook', phonebookSchema);
+
+const argNum = process.argv.length;
+
+if (argNum == 3) {
+    Phonebook.find({}).then(res => {
+        console.log('phonebook:')
+        res.forEach((pb) => {
+            console.log(`${pb.name} ${pb.number}`);
+        });
+    }).catch((err) => {
+        console.log(err);        
+    }).finally(() => {
+        mongoose.connection.close();
+    });
+} else {
+    const name = process.argv[3];
+    const number = process.argv[4];
+    const phonebook = new Phonebook({
+        name,
+        number,
+    });
+    phonebook.save().then(response => {
+        console.log(`added ${name} number ${number} to phonebook`);
+    }).catch((err) => {
+        console.log(err);
+    }).finally(() => {
+        mongoose.connection.close();
+    });
+}
