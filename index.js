eee457224db7465711ffe27093f3f7034d059902
Jarkko Mourujärvi <jarkko@mourujarvi.net>
2020-05-02 20:46:43 +0300

    3.15: puhelinluettelo ja tietokanta, step3

--- index.js
+++ index.js
@@ -20,29 +20,6 @@ app.use(morgan((tokens, req, res) => {
   ].join(' ');
 }));
 
-let persons = [
-  {
-    "name": "Arto Hellas",
-    "number": "040-123456",
-    "id": 1
-  },
-  {
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523",
-    "id": 2
-  },
-  {
-    "name": "Dan Abramov",
-    "number": "12-43-234345",
-    "id": 3
-  },
-  {
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122",
-    "id": 4
-  }
-];
-
 app.get('/info', (req, res) => {
   res.contentType('text/plain');
   
@@ -59,15 +36,9 @@ app.get('/api/persons', (req, res) => {
 });
 
 app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id);
-
-  const person = persons.find(person => person.id === id);
-  if (!person) {
-    return res.status(404).end();
-
-  }
-
-  res.json(person);
+  const person = Phonebook.findById(req.params.id).then((person) => {
+    res.json(person.toJSON());
+  });
 });
 
 app.post('/api/persons', (req, res) => {
@@ -113,11 +84,11 @@ app.post('/api/persons', (req, res) => {
 
 
 app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id);
+  const id = req.params.id;
 
-  persons = persons.filter(person => person.id !== id);
-
-  res.sendStatus(204);
+  Phonebook.findByIdAndRemove(id).then((result) => {
+    res.sendStatus(204);
+  });
 });
 
 const PORT = process.env.PORT || 3001;
